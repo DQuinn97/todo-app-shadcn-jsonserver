a53a1d68a68993eb9d7c8669635d15aae2aa4670
DQuinn97 <44023796+DQuinn97@users.noreply.github.com>
2025-03-19 22:32:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const cors = require('cors');
+const path = require('path');
 
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000; 
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 server.listen(port);
